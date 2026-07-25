b67d107a0c6627cd9fff0e97952efd85eca18fa0
HibernalGlow <3090253564@qq.com>
2026-07-25 16:19:50 +0800

    fix: expose player integration entrypoints

--- packages/player/src/index.ts
+++ packages/player/src/index.ts
@@ -2,9 +2,19 @@ export { FoliaPlayerProvider, useFoliaPlayer } from './PlayerProvider';
 export { FoliaRemoteSurface, type FoliaRemoteSurfaceProps } from './RemoteSurface';
 export { FoliaBarSurface, type FoliaBarSurfaceProps } from './BarSurface';
 export { FoliaUnifiedPanel, type FoliaUnifiedPanelProps, type FoliaPanelTab } from './UnifiedPanel';
+export { FoliaFullscreenSurface, type FoliaFullscreenSurfaceProps } from './FullscreenSurface';
 export { buildFoliaDualTheme, DEFAULT_FOLIA_DUAL_THEME, type FoliaThemeTokens } from './theme';
 export { DEFAULT_FOLIA_PLAYER_PREFERENCES } from './types';
 export { foliaPlayerLocales } from './locales';
+export {
+    parseLyricsByFormat,
+    parseLRC,
+    parseYRC,
+    parseQRC,
+    parseKRC,
+    type LyricData,
+    type LyricParseFormat,
+} from './parser';
 export type {
     FoliaLoopMode,
     FoliaMotionRuntime,
